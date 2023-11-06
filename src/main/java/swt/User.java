b0b325c10059b8f5de71b8595910a496eb5ce786
JoannisKazakis <149683486+JoannisKazakis@users.noreply.github.com>
2023-11-06 13:02:07 +0000

    Yes

--- src/main/java/swt/User.java
+++ src/main/java/swt/User.java
@@ -20,4 +20,37 @@ public class User {
     public void setUsername(String newUsername) {
         this.username = newUsername;
     }
+
+    public class UserAuthentication {
+        private String username;
+        private String password;
+        private boolean isLoggedIn;
+
+        // Konstruktor
+        public UserAuthentication(String username, String password) {
+            this.username = username;
+            this.password = password;
+            this.isLoggedIn = false; // Initialer Anmeldestatus ist "ausgeloggt"
+        }
+
+        // Methode zum Einloggen
+        public void logIn() {
+            if (username.equals("Eingabe JavaFX") && password.equals("Eingabe JavaFX")) {
+                isLoggedIn = true;
+            } else {
+                isLoggedIn = false; //Falsche Anmeldedaten, bitte nochmal einloggen
+            }
+        }
+
+        // Methode zum Ausloggen
+        public void logOut() {
+            isLoggedIn = false; //  zurück zur Anmeldemaske
+        }
+
+        // Getter-Methode für den Anmeldestatus
+        public boolean isUserLoggedIn() {
+            return isLoggedIn;  //Anzeige in der Übersichtsmaske: Wer ist eingeloggt.
+        }
+
+    }
 }
